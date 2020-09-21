414b2bffefd7d19f8d66fdd862d2fa25eb8db5d4
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-09-21 22:15:59 +0300

    mycp progress

--- lsn3/mycp.c
+++ lsn3/mycp.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <getopt.h>
+#include <ctype.h>
 
 #define BUFFER_SIZE 4096
-#define SHORT_OPTS "fiv"
 
 // Keys bits constants
 #define F_KEY 0x1
 #define I_KEY 0x2
 #define V_KEY 0x4
+
+// useful typedef
+typedef struct option OPT;
+
+const OPT OPTIONS[] =
+  {
+    {"force", 0, NULL, 'f'},
+    {"interactive", 0, NULL, 'i'},
+    {"verbose", 0, NULL, 'v'},
+  };
+const char OPT_STR[] = "fiv";
 
 __inline int MyErr( char *str_err ) __attribute__((always_inline));
 
@@ -42,11 +56,40 @@ int MyWrite( int fd, void *buffer, size_t buf_size )
 
 int GetOptions( int argc, char *argv )
 {
+  int getopt_ret = 0;
+  int flag = 0;
 
+  while (1)
+  {
+    getopt_ret = getopt_long(argc, argv, OPT_STR, OPTIONS);
+
+    switch (getopt_ret)
+    {
+    case -1:
+        return flag;
+    case '?':
+      printf("Unrecognized option: \"%s\"\n", argv[optind])
+      return -1;
+    case 'f':
+      flag |= F_KEY;
+      break;
+    case 'i':
+      flag |= I_KEY;
+      break;
+    case 'v':
+      flag |= V_KEY;
+      break;
+    default:
+      printf("Unrecognized return value: %d\n", getopt_ret);
+    }
+  }
 }
 
 int main( int argc, char *argv[] )
 {
-  if
+  int flags = GetOptions(argc, argv);
 
+
+
+  return 0;
 }
