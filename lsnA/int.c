b654a671a3b92f28d69f50c5e557a3275aabe1d5
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-11-13 15:03:28 +0300

    progress

--- lsnA/int.c
+++ lsnA/int.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 void Print( int x )
 {
   printf("Hello\n");
 }
 
+void Resize( int x )
+{
+  // TODO: draw frame on screen by '*'
+  struct winsize win;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
+  printf("cols = %d\nrows = %d\n", win.ws_col, win.ws_row);
+}
+
 int main( void )
 {
   signal(SIGINT, Print);
-
-  while (1)
-    pause();
+  signal(SIGWINCH, Resize);
+  pause();
 }
