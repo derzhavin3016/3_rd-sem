d60127f701d61d1b04528885a6eb00c83e333508
andrey <derzhavin.andrej@yandex.ru>
2020-12-04 01:39:29 +0300

    upd

--- lsnC/epoll.c
+++ lsnC/epoll.c
@@ -86,16 +86,29 @@ int main( int argc, char *argv[] )
   int pipe_fds[forks_num][2][2];
   int fd_in  = open(argv[1], O_RDONLY, MAX_ACCESS),
       fd_out = open(argv[2], O_WRONLY | O_CREAT, MAX_ACCESS);
+      
+  // Creating epoll
+  int epoll_fd = epoll_create1(0);
+        
   CHECK_DF( fd_in, argv[1]);
   CHECK_DF(fd_out, argv[2]);
-  
+  struct epoll_event events[forks_num - 1];
   // creating pipes 
   for (int i = 0; i < forks_num; ++i)
-  {
+  {  
     CHECK_DF(pipe(pipe_fds[i][0]), "pipe create error");
     CHECK_DF(pipe(pipe_fds[i][1]), "pipe create error");
+    if (i != forks_num - 1)
+    {
+      events[i].events = EPOLLIN;
+      events[i].data.fd = pipe_fds[i + 1][1][1];
+      CHECK_DF(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipe_fds[i][0][0], events + i),
+               "ctl error");
+    }
+    
   }
   
+  // attach the INPUT and OUTPUT fds to pipe's array (for easier usage)
   CHECK_DF(dup2(fd_in, pipe_fds[0][1][0]), "dup error");
   CHECK_DF(dup2(fd_out, pipe_fds[forks_num - 1][0][1]), "dup error");
   
@@ -106,7 +119,15 @@ int main( int argc, char *argv[] )
     CHECK_DF(cpid, "fork error");
     
     if (cpid == 0)
-      return ReadWriteFile(pipe_fds[i][1][0], pipe_fds[i][0][1]);
+    {
+      int fd_src = pipe_fds[i][1][0],
+          fd_dst = pipe_fds[i][0][1];
+          
+      CHECK(ReadWriteFile(fd_src, fd_dst) == 0, "RW err");
+      CHECK_DF(close(fd_src), "error closing src");
+      CHECK_DF(close(fd_dst), "error closing dst");
+      return 0;
+    }
   } 
   
 
@@ -120,5 +141,6 @@ int main( int argc, char *argv[] )
     CHECK_DF(close(pipe_fds[i][1][1]), "pipe close error");
   }  
 
+  CHECK_DF(close(epoll_fd), "epoll closing error");
 	return 0;
 }
