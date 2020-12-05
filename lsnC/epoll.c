9e9713df6dfa4e5bb658da2b1d14b9aaa036e2c0
andrey <derzhavin.andrej@yandex.ru>
2020-12-05 21:05:10 +0300

    some fixes

--- lsnC/epoll.c
+++ lsnC/epoll.c
@@ -9,7 +9,6 @@
 #include <fcntl.h>
 
 #define MAX_ACCESS 0777
-#define QUEUE_NAME "/shmque"
 #define BUFFER_SIZE 4096
 
 #define CHECK(cond, err_str) \
@@ -72,27 +71,52 @@ int GetFlength( int fd )
   return fstat(fd, &buf) == -1 ? -1 : buf.st_size;
 }
 
+int ForksProcess( void )
+{
+
+}
 
 int forks_num = 0;
 
+/**!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *      IMPORTANT!!!!!!!
+ ************************************
+ *      pipe fds- ---- 3-dim array
+ *      parent has an access to all of that pipes
+ *      the num config is:
+ *      pipe_fds[     ]                            [     ]                                     [     ]
+ *                 ^                                  ^                                           ^
+ *            num of child       0 - parent read can read from this pipe                        0 - read fd
+ *         (each has 2 pipes)    1 - parent read can write to this pipe                         1 - write fd
+ *  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
 int main( int argc, char *argv[] )
 {
+  // argc argv processing
   if (argc != 4)
   {
     printf("USAGE: %s INPUT OUTPUT N_OF_PROC\n", argv[0]);
     return 0;
   }
+  ///////////////////////////////////////////
+
+  // getting arguments, open files
   forks_num = atoi(argv[3]);
   int pipe_fds[forks_num][2][2];
   int fd_in  = open(argv[1], O_RDONLY, MAX_ACCESS),
       fd_out = open(argv[2], O_WRONLY | O_CREAT, MAX_ACCESS);
-      
-  // Creating epoll
-  int epoll_fd = epoll_create1(0);
-        
+
+  // check if files open was ok
   CHECK_DF( fd_in, argv[1]);
   CHECK_DF(fd_out, argv[2]);
+
+  // Creating epoll & check valid
+  int epoll_fd = epoll_create1(0);
+  CHECK_DF(epoll_fd, "epoll_create1");
+
+  // creating an array for events
   struct epoll_event events[forks_num - 1];
+
   // creating pipes 
   for (int i = 0; i < forks_num; ++i)
   {  
@@ -111,41 +135,47 @@ int main( int argc, char *argv[] )
   // attach the INPUT and OUTPUT fds to pipe's array (for easier usage)
   CHECK_DF(dup2(fd_in, pipe_fds[0][1][0]), "dup error");
   CHECK_DF(dup2(fd_out, pipe_fds[forks_num - 1][0][1]), "dup error");
-  
+
+  // creating processes
   for (int i = 0; i < forks_num; ++i)
   {
     pid_t cpid = fork();
-    
+
     CHECK_DF(cpid, "fork error");
-    
+
     if (cpid == 0)
     {
       int fd_src = pipe_fds[i][1][0],
           fd_dst = pipe_fds[i][0][1];
-          
+
       CHECK(ReadWriteFile(fd_src, fd_dst) == 0, "RW err");
       CHECK_DF(close(fd_src), "error closing src");
       CHECK_DF(close(fd_dst), "error closing dst");
       return 0;
     }
-  } 
-  
+  }
+  ///////////////////////////////////////////
+
   // Here parent resell data between pipes
-  for (int i = 0; i < forsk_num; ++i)
+  for (int i = 0; i < forks_num; ++i)
   {
-    struct epoll_event ev;      
-    int n = epoll_wait(epoll_fd, &ev, forks_num, -1);
-    ReadWriteFile(pipe_fds[i][][], )  
+    struct epoll_event ev;
+    int n = 0;
+    do
+    {
+      n = epoll_wait(epoll_fd, &ev, forks_num, -1);
+      ReadWriteFile(pipe_fds[i][][],)
+    } while (n);
   }
   
   
-
+  ///// NEAR THE END ///////////////////////////////////
   for (int i = 0; i < forks_num; ++i)
   {
     // waiting for child process
     wait(NULL);
     
-    // closing parents's pipes
+    // closing parent's pipes
     CHECK_DF(close(pipe_fds[i][0][0]), "pipe close error");
     CHECK_DF(close(pipe_fds[i][1][1]), "pipe close error");
   }  
